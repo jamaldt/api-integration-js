6418682b137d8e479fbfcec9cf47feec949ecc24
jamal <carldeltoro@gmail.com>
2022-03-10 11:25:48 -0400

    getDatos promise

---
+++ promises/asincronia.js
@@ -0,0 +1,24 @@
+const datos = [
+    {
+        id:1,
+        nombre:"goku",
+        pdoer:"genkidama"
+    },
+    {
+        id:2,
+        nombre:"freezer",
+        pdoer:"death ball"
+    },
+    {
+        id:3,
+        nombre:"piccolo",
+        pdoer:"masenko"
+    }
+]
+
+
+const getDatos = ()=>{
+    return datos;
+}
+
+console.log(getDatos());
